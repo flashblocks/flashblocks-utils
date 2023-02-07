5fe11bd7c4b0e24230d4db03e193b8454cb6aab3
Sunny <Sunny@sunnymorgan.com>
2023-02-06 19:05:23 -0500

    version 1.0.7

--- src/Flashblocks/Utils/Enqueue.php
+++ src/Flashblocks/Utils/Enqueue.php
@@ -67,13 +67,12 @@ class Enqueue {
 	 *                                             If set to null, no version is added.
 	 * @param bool             $footer_or_media    Optional. Whether to enqueue the script before `</body>` instead of in the `<head>`.
 	 */
-	public function __construct( $handle = null, $src = null, $deps = null, $ver = null, $footer_or_media = null, $inline = false ) {
+	public function __construct( $handle = null, $src = null, $deps = null, $ver = null, $footer_or_media = null ) {
 		$this->handle          = $handle;
 		$this->src             = $src;
 		$this->deps            = $deps;
 		$this->ver             = $ver;
 		$this->footer_or_media = $footer_or_media;
-		$this->inline          = $inline;
 
 		return $this;
 	}
@@ -90,6 +89,7 @@ class Enqueue {
 
 		return $this;
 	}
+
 
 
 	public function start() {

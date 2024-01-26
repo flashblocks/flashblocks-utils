f802e88721c046d92907fcab353a94b31e0e2ffb
Sunny <Sunny@sunnymorgan.com>
2024-01-26 18:49:51 -0500

    start on tax list

---
+++ src/taxonomy-list/render.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * @var array $attributes Block attributes.
+ * @var string $content Block default content.
+ * @var \WP_Block $block Block instance.
+ *
+ * @package wpe/slider-block
+ */
+
+d($attributes);
+ddd($content);
+ddd($block);
+echo '11111';
+

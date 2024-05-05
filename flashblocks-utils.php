c2470e34a34a3f0912ff5cd5e94d5632ec980a4c
Sunny <Sunny@sunnymorgan.com>
2024-05-05 11:22:33 -0700

    composer

--- flashblocks-utils.php
+++ flashblocks-utils.php
@@ -4,7 +4,7 @@
  * Description:       Utils and blocks that are instantiated with new Classname
  * Requires at least: 6.0
  * Requires PHP:      7.4.0
- * Version:           1.0.13
+ * Version:           1.0.1
  * Author:            Sunny Morgan
  * Text Domain:       flashblocks
  *
@@ -15,7 +15,7 @@ namespace Flashblocks\Utils;
 
 if ( ! defined( 'WPINC' ) ) die;
 
-const VERSION = '1.0.0';
+const VERSION = '1.0.1';
 const DIR     = __DIR__;
 const FILE    = __FILE__;
 define( __NAMESPACE__ . '\URL', plugin_dir_url( __FILE__ ) );
---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'flashblocks/utils',
+        'pretty_version' => '2.0.0',
+        'version' => '2.0.0.0',
+        'reference' => NULL,
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => false,
+    ),
+    'versions' => array(
+        'flashblocks/utils' => array(
+            'pretty_version' => '2.0.0',
+            'version' => '2.0.0.0',
+            'reference' => NULL,
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
